d43c344a3ac8ae1cec5d8dac252668a10cb0a5ff
felix.gateru <felix.gateru@gmail.com>
2024-02-05 17:02:53 +0300

    Revert config urls
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -29,14 +29,14 @@ type config struct {
 	LogLevel        string          `env:"MG_UI_LOG_LEVEL"        envDefault:"debug"`
 	Port            string          `env:"MG_UI_PORT"             envDefault:"9095"`
 	InstanceID      string          `env:"MG_UI_INSTANCE_ID"      envDefault:""`
-	HTTPAdapterURL  string          `env:"MG_HTTP_ADAPTER_URL"    envDefault:"http://http-adapter:8008"`
-	ReaderURL       string          `env:"MG_READER_URL"          envDefault:"http://mongodb-reader:9007"`
-	ThingsURL       string          `env:"MG_THINGS_URL"          envDefault:"http://things:9000"`
-	UsersURL        string          `env:"MG_USERS_URL"           envDefault:"http://users:9002"`
+	HTTPAdapterURL  string          `env:"MG_HTTP_ADAPTER_URL"    envDefault:"http://localhost:8008"`
+	ReaderURL       string          `env:"MG_READER_URL"          envDefault:"http://localhost:9007"`
+	ThingsURL       string          `env:"MG_THINGS_URL"          envDefault:"http://localhost:9000"`
+	UsersURL        string          `env:"MG_USERS_URL"           envDefault:"http://localhost:9002"`
 	HostURL         string          `env:"MG_UI_HOST_URL"         envDefault:"http://localhost:9095"`
-	BootstrapURL    string          `env:"MG_BOOTSTRAP_URL"       envDefault:"http://bootstrap:9013"`
-	DomainsURL      string          `env:"MG_DOMAINS_URL"         envDefault:"http://domains:8189"`
-	InvitationsURL  string          `env:"MG_INVITATIONS_URL"     envDefault:"http://invitations:9020"`
+	BootstrapURL    string          `env:"MG_BOOTSTRAP_URL"       envDefault:"http://localhost:9013"`
+	DomainsURL      string          `env:"MG_DOMAINS_URL"         envDefault:"http://localhost:8189"`
+	InvitationsURL  string          `env:"MG_INVITATIONS_URL"     envDefault:"http://localhost:9020"`
 	MsgContentType  sdk.ContentType `env:"MG_UI_CONTENT_TYPE"     envDefault:"application/senml+json"`
 	TLSVerification bool            `env:"MG_UI_VERIFICATION_TLS" envDefault:"false"`
 }
