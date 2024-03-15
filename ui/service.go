592ff8394e6af9f2a379100105da94b9277dfa16
ianmuchyri <ianmuchiri8@gmail.com>
2024-03-15 10:07:19 +0300

    rename variable to MilliToNanoRatio
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- ui/service.go
+++ ui/service.go
@@ -152,12 +152,12 @@ var (
 	ErrFailedDashboardUpdate   = errors.New("failed to update dashboard")
 	ErrFailedDashboardDelete   = errors.New("failed to delete dashboard")
 
-	domainRelations      = []string{"administrator", "editor", "viewer", "member"}
-	groupRelations       = []string{"administrator", "editor", "viewer"}
-	statusOptions        = []string{"all", "enabled", "disabled"}
-	uuidPattern          = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$"
-	intervalPattern      = "^([0-9][0-9]*[smhd])$"
-	MilliToNanoConverter = math.Pow10(6)
+	domainRelations  = []string{"administrator", "editor", "viewer", "member"}
+	groupRelations   = []string{"administrator", "editor", "viewer"}
+	statusOptions    = []string{"all", "enabled", "disabled"}
+	uuidPattern      = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$"
+	intervalPattern  = "^([0-9][0-9]*[smhd])$"
+	MilliToNanoRatio = 1e6
 )
 
 // Service specifies service API.
@@ -1720,7 +1720,7 @@ func (us *uiService) ReadMessages(s Session, channelID, thingKey string, mpgm sd
 	}
 
 	for i := 0; i < len(msg.Messages); i++ {
-		msg.Messages[i].Time = msg.Messages[i].Time / MilliToNanoConverter
+		msg.Messages[i].Time = msg.Messages[i].Time / MilliToNanoRatio
 	}
 
 	noOfPages := int(math.Ceil(float64(msg.Total) / float64(mpgm.Limit)))
@@ -1770,7 +1770,7 @@ func (us *uiService) FetchChartData(token string, channelID string, mpgm sdk.Mes
 	}
 
 	for i := 0; i < len(msg.Messages); i++ {
-		msg.Messages[i].Time = msg.Messages[i].Time / MilliToNanoConverter
+		msg.Messages[i].Time = msg.Messages[i].Time / MilliToNanoRatio
 	}
 
 	data, err := json.Marshal(msg)
